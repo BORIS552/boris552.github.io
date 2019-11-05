96d188d145c48ddc62b2b53ea522ee772fc468ce
BORIS552 <boris.biswas@gmail.com>
2019-11-05 23:16:25 +0530

    Initial commit

---
+++ precache-manifest.32e1cd5976ad759507236ca7ad048854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5498ed0e17a1f99bc8fc2f46a95bfe88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "743427e2471b782fb18b",
+    "url": "/static/css/main.247e8916.chunk.css"
+  },
+  {
+    "revision": "95ff53eb9de458e6e4a3",
+    "url": "/static/js/2.eef0d318.chunk.js"
+  },
+  {
+    "revision": "743427e2471b782fb18b",
+    "url": "/static/js/main.56d53655.chunk.js"
+  },
+  {
+    "revision": "fe86899796dad68f7dfb",
+    "url": "/static/js/runtime-main.a6da9dfb.js"
+  },
+  {
+    "revision": "f7f12d8b700f6d8f96ca0c84a7a366e3",
+    "url": "/static/media/logo.f7f12d8b.svg"
+  }
+]);
